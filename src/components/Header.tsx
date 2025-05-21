afeeb4f20b67808a154620056d2dbb85b5897779
pir4a <piralith@outlook.fr>
2025-05-21 23:24:11 +0200

    logo again i love it

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,8 +7,8 @@ function Header() {
     <div className='sticky top-6 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
       <div className='container flex h-16 items-center justify-between'>
         <nav className='flex items-center gap-2 px-12'>
-          <div className='flex gap-6 justify-between items-center'>
-          <img src={fissureIcon} alt="Fissure Icon" className='h-14 w-14 animate-electricpulse duration-[]'/>
+          <div className='flex gap-6 justify-between items-center '>
+          <img src={fissureIcon} alt="Fissure Icon" className='h-14 w-14  animate-electricpulse'/>
           <GlitchText speed={2} enableShadows={true} enableOnHover={false}>Fissure</GlitchText></div>
         </nav>
       </div>
