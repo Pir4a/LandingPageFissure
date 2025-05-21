ceecbb054a99d2c53ef8881acb7938deb2772edf
pir4a <piralith@outlook.fr>
2025-05-21 20:24:45 +0200

    header icon & glitchy text adjustments

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,10 +6,11 @@ function Header() {
   return (
     <div className='sticky top-6 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
       <div className='container flex h-16 items-center justify-between'>
-        <div className='flex items-center gap-2'>
-          <img src={fissureIcon} alt="Fissure Icon" className='h-12 w-12 animate-electricpulse duration-[]'/>
-          <GlitchText speed={1.8} enableShadows={true} enableOnHover={false}>Fissure</GlitchText>
-        </div>
+        <nav className='flex items-center gap-2 px-12'>
+          <div className='flex gap-6 justify-between items-center'>
+          <img src={fissureIcon} alt="Fissure Icon" className='h-14 w-14 animate-electricpulse duration-[]'/>
+          <GlitchText speed={2} enableShadows={true} enableOnHover={false}>Fissure</GlitchText></div>
+        </nav>
       </div>
     </div>
   )
