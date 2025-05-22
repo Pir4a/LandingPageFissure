ac285d088f1512c8ab5f2220c8510227749d7fc8
pir4a <piralith@outlook.fr>
2025-05-22 11:50:30 +0200

    background and better sizes

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Hero from './components/Hero'
 
 function App() {
   return (
-    <div className='h-full min-h-[150dvh] bg-black'>
-    <Header/>
-    <Hero/>
-    <Hero/>
-    <Hero/>
-</div>  )
+    <div className='h-full min-h-[150dvh] bg-gradient-to-b from-gray-950 via-gray-900 to-black'>
+      <div className='absolute inset-0 bg-[radial-gradient(circle_at_center,rgba(99,102,241,0.15),transparent_50%)]'></div>
+      <Header/>
+      <Hero/>
+      <Hero/>
+      <Hero/>
+    </div>
+  )
 }
 
 export default App
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import qrcode from '../assets/qrcode.png'
 function Hero() {
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 bg-gradient-to-b from-background to-muted  text-white">
+    <section className="w-full xl:pt-[20%] py-12 md:py-24 lg:py-32 xl:py-48 bg-gradient-to-b from-background to-muted  text-white">
           <div className=" w-3/4 mx-auto flex items-center justify-center x-4 md:px-6">
             <div className="flex xl:flex-row flex-col gap-8">
               <div className="flex flex-col justify-center space-y-4">
@@ -37,13 +37,18 @@ function Hero() {
                   </div>
                 </div>
               </div>
-                <div className="relative flex justfiy-center items-center h-[450px] w-[300px]  rounded-xl shadow-2xl sm:w-[350px] md:h-[600px] md:w-[380px]">
-                  <img
-                    alt="App screenshot"
-                    src={qrcode} 
-                    className="object-cover"
-                  />
-                  <div className="absolute z-40 top-30 -left-8 w-16 h-16">
+                <div className="relative flex justfiy-center items-center h-[450px] w-[300px]  rounded-xl shadow-2xl sm:w-[350px] md:h-[260px] md:w-[260px]">
+                  <div className="relative w-[260px] h-[260px] rounded-xl overflow-hidden">
+                    <div className="absolute brightness-125 inset-0 bg-gradient-to-r from-purple-900 via-indigo-900 to-blue-900 animate-pulse  rounded-xl"></div>
+                    <div className="absolute inset-[3px] bg-background rounded-xl">
+                      <img
+                        alt="App screenshot"
+                        src={qrcode} 
+                        className="object-cover rounded-xl"
+                      />
+                    </div>
+                  </div>
+                  <div className="absolute z-40 -top-12 -left-5 w-16 h-16">
                     <div className="relative w-full h-full rotate-[-30deg]">
                       <h2 className=' absolute -top-5 left-25 w-[150px] rotate-20'>Scan to get APK !</h2>
                           <div className="absolute w-8 h-8 border-t-4 border-r-4 border-white rounded transform rotate-[130deg] translate-x-1 -translate-y-1"></div>
