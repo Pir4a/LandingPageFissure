f248050e3ebabf7c9ef9a1b54a489998a7bcd2be
pir4a <piralith@outlook.fr>
2025-05-28 19:37:36 +0200

    custom breakpoint for appscreens

--- src/components/AppScreans.tsx
+++ src/components/AppScreans.tsx
@@ -6,7 +6,7 @@ function AppScreans() {
   return (
     <div className='flex rounded-xl gap-12 '>
         <img className='w-[350px] mx-auto xl:mx-0 xl:w-[500px] h-[680px] 2xl:h-[700px] rounded-3xl ' src={screenTwo} />
-        <img className='w-[450px] h-[700px] rounded-3xl hidden 2xl:block' src={screenOne} />
+        <img className='w-[450px] h-[700px] rounded-3xl hidden [@media(min-width:1700px)]:block' src={screenOne} />
     </div>
   )
 }
